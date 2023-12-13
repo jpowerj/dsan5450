4294088318193e4635faf7920f9612090ea4aac0
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2023-12-12 23:44:42 -0500

    Corrected syllabus dates

---
+++ jjcustom.js
@@ -0,0 +1,5 @@
+window.document.addEventListener("DOMContentLoaded", function (event) {
+    var $subtitleDiv = $("<br><span class='subtitle'>Weds, 3:30-6pm</span><br><span class='subtitle'>Walsh 498</span><br><span class='subtitle'>Prof. Jeff Jacobs&nbsp;<a href='mailto:jj1088@georgetown.edu' target='_blank'><i class='bi bi-envelope-at ps-1 pe-1'></i></a></span><br><span class='subtitle'>Spring 2024, Georgetown</span>");
+    $('.sidebar-title').append($subtitleDiv);
+  });
+  
